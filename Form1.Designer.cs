15e8c22e22ef25887f6b2f2a56a4d5ec389506aa
vtopilskiy <vtopilskiy@at-consulting.com>
2025-01-22 20:37:26 +0300

    Parallel sort

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Topilskiy_Task1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            originalNumberListBox = new ListBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            numericUpDown2 = new NumericUpDown();
+            label2 = new Label();
+            label3 = new Label();
+            bubleSortListBox = new ListBox();
+            bubleSortLabel = new Label();
+            insertionSortListBox = new ListBox();
+            insertionSortLabel = new Label();
+            countingSortListBox = new ListBox();
+            countingSortLabel = new Label();
+            label7 = new Label();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(7, 230);
+            button1.Name = "button1";
+            button1.Size = new Size(127, 54);
+            button1.TabIndex = 0;
+            button1.Text = "Сгенерировать и отсортировать";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            originalNumberListBox.FormattingEnabled = true;
+            originalNumberListBox.ItemHeight = 15;
+            originalNumberListBox.Location = new Point(143, 44);
+            originalNumberListBox.Name = "listBox1";
+            originalNumberListBox.Size = new Size(52, 604);
+            originalNumberListBox.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(34, 143);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "1000";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(34, 125);
+            label1.Name = "label1";
+            label1.Size = new Size(96, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Размер массива";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(35, 172);
+            numericUpDown1.Maximum = new decimal(new int[] { 2147483640, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(99, 23);
+            numericUpDown1.TabIndex = 4;
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(35, 201);
+            numericUpDown2.Maximum = new decimal(new int[] { 2147483640, 0, 0, 0 });
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(99, 23);
+            numericUpDown2.TabIndex = 5;
+            numericUpDown2.Value = new decimal(new int[] { 100, 0, 0, 0 });
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(8, 180);
+            label2.Name = "label2";
+            label2.Size = new Size(21, 15);
+            label2.TabIndex = 6;
+            label2.Text = "От";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(7, 209);
+            label3.Name = "label3";
+            label3.Size = new Size(22, 15);
+            label3.TabIndex = 7;
+            label3.Text = "До";
+            // 
+            // listBox2
+            // 
+            bubleSortListBox.FormattingEnabled = true;
+            bubleSortListBox.ItemHeight = 15;
+            bubleSortListBox.Location = new Point(306, 45);
+            bubleSortListBox.Name = "listBox2";
+            bubleSortListBox.Size = new Size(52, 604);
+            bubleSortListBox.TabIndex = 8;
+            // 
+            // label4
+            // 
+            bubleSortLabel.AutoSize = true;
+            bubleSortLabel.Location = new Point(363, 44);
+            bubleSortLabel.Name = "label4";
+            bubleSortLabel.Size = new Size(204, 15);
+            bubleSortLabel.TabIndex = 9;
+            bubleSortLabel.Text = "Сортировка пузырьком завершена!";
+            bubleSortLabel.Visible = false;
+            // 
+            // listBox3
+            // 
+            insertionSortListBox.FormattingEnabled = true;
+            insertionSortListBox.ItemHeight = 15;
+            insertionSortListBox.Location = new Point(573, 44);
+            insertionSortListBox.Name = "listBox3";
+            insertionSortListBox.Size = new Size(52, 604);
+            insertionSortListBox.TabIndex = 12;
+            // 
+            // label5
+            // 
+            insertionSortLabel.AutoSize = true;
+            insertionSortLabel.Location = new Point(631, 45);
+            insertionSortLabel.Name = "label5";
+            insertionSortLabel.Size = new Size(199, 15);
+            insertionSortLabel.TabIndex = 14;
+            insertionSortLabel.Text = "Сортировка вставками завершена!";
+            insertionSortLabel.Visible = false;
+            // 
+            // listBox4
+            // 
+            countingSortListBox.FormattingEnabled = true;
+            countingSortListBox.ItemHeight = 15;
+            countingSortListBox.Location = new Point(836, 45);
+            countingSortListBox.Name = "listBox4";
+            countingSortListBox.Size = new Size(52, 604);
+            countingSortListBox.TabIndex = 15;
+            // 
+            // label6
+            // 
+            countingSortLabel.AutoSize = true;
+            countingSortLabel.Location = new Point(891, 45);
+            countingSortLabel.Name = "label6";
+            countingSortLabel.Size = new Size(136, 15);
+            countingSortLabel.TabIndex = 16;
+            countingSortLabel.Text = "Сортировка подсчётом";
+            countingSortLabel.Visible = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(143, 24);
+            label7.Name = "label7";
+            label7.Size = new Size(162, 15);
+            label7.TabIndex = 18;
+            label7.Text = "Сгенерированные значения";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1149, 655);
+            Controls.Add(label7);
+            Controls.Add(countingSortLabel);
+            Controls.Add(countingSortListBox);
+            Controls.Add(insertionSortLabel);
+            Controls.Add(insertionSortListBox);
+            Controls.Add(bubleSortLabel);
+            Controls.Add(bubleSortListBox);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(numericUpDown2);
+            Controls.Add(numericUpDown1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(originalNumberListBox);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Параллельная сортировка разными алгоритмами";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private ListBox originalNumberListBox;
+        private TextBox textBox1;
+        private Label label1;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private Label label2;
+        private Label label3;
+        private ListBox bubleSortListBox;
+        private Label bubleSortLabel;
+        private ListBox insertionSortListBox;
+        private Label insertionSortLabel;
+        private ListBox countingSortListBox;
+        private Label countingSortLabel;
+        private Label label7;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,289 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Topilskiy_Task1
+{
+    public partial class Form1 : Form
+    {
+        List<int>? numbers = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            AddButtonListener();
+        }
+
+        private void AddButtonListener()
+        {
+            button1.Click += Button1_Click;
+        }
+
+        // Генерация чисел и запуск сортировок.
+        private void Button1_Click(object? sender, EventArgs e)
+        {
+            // Определение размера массива и диапазона чисел
+            int minBound = (int)numericUpDown1.Value;
+            int maxBound = (int)numericUpDown2.Value;
+            String sizeStr = this.textBox1.Text;
+            int size;
+            bool parseResult = Int32.TryParse(sizeStr, out size);
+            if (!parseResult)
+            {
+                MessageBox.Show("Ошибка! Введён неправильный размер масива.");
+                return;
+            }
+            if (size <= 0)
+            {
+                MessageBox.Show("Ошибка! Введённый размер массива меньше или равен нулю.");
+                return;
+            }
+            if (minBound > maxBound)
+            {
+                MessageBox.Show("Ошибка! Введённый неверный диапазон чисел.");
+                return;
+            }
+
+            // Очищиаем ListBox от предыдущих значений
+            originalNumberListBox.DataSource = null;
+            originalNumberListBox.Items.Clear();
+
+            // Скрытие сообщений об окончании прошлой сортировки
+            bubleSortLabel.Invoke(new Action(() => bubleSortLabel.Visible = false));
+            insertionSortLabel.Invoke(new Action(() => insertionSortLabel.Visible = false));
+            countingSortLabel.Invoke(new Action(() => countingSortLabel.Visible = false));
+
+            // Генерация массива
+            numbers = new List<int>(size);
+            Random random = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                int n = random.Next(minBound, maxBound);
+                numbers.Add(n);
+            }
+
+            // Вывод массива в ListBox
+            originalNumberListBox.Invoke(new Action(() =>
+            {
+                originalNumberListBox.DataSource = numbers;
+            }));
+
+            // Запуск сортировок
+            BubleSort();
+            InsertionSort();
+            CountingSort();
+        }
+
+        //Сортировка пузырьком
+        private void BubleSort()
+        {
+            // Определение делегата сортировки
+            Task bubleSort = new Task(() =>
+            {
+                // Очищение ListBox от предыдущих значений
+                bubleSortListBox.DataSource = null;
+                bubleSortListBox.Invoke(new Action(() => bubleSortListBox.Items.Clear()));
+
+                // Создание нового массива, чтобы в него скопировать сгенерированные числа и в нём совершить сортировку
+                int[] sortedArray = new int[numbers?.Count ?? 0];
+                numbers?.CopyTo(sortedArray);
+
+                // Включение секундомера
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                // Переменные для подсчёта количества сравнений и перестановок
+                long compareCount = 0;
+                long moveCount = 0;
+
+                // Непосредственно сортировка
+                int n = sortedArray.Length;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    for (int j = 0; j < n - i - 1; j++)
+                    {
+                        compareCount++;
+                        if (sortedArray[j] > sortedArray[j + 1])
+                        {
+                            int tempVar = sortedArray[j];
+                            sortedArray[j] = sortedArray[j + 1];
+                            sortedArray[j + 1] = tempVar;
+                            moveCount++;
+                        }
+                    }
+                }
+
+                // Остановка секундомера
+                stopwatch.Stop();
+
+                // Вывод сообщения о завершении сортировки и собранных показателях
+                bubleSortLabel.Invoke(new Action(() =>
+                {
+                    bubleSortLabel.Text = "Сортировка пузырьком завершена!\n" +
+                    "Кол-во сравнений: " + compareCount + ". \n" +
+                    "Кол-во перестановок: " + moveCount + ". \n" +
+                    "Затрачено времени: " + stopwatch.ElapsedMilliseconds + " мс.";
+                    bubleSortLabel.Visible = true;
+                }));
+
+                // Вывод отсортированного массива
+                bubleSortListBox.Invoke(new Action(() => bubleSortListBox.DataSource = sortedArray));
+            });
+
+            // Запуск сортировки
+            bubleSort.Start();
+        }
+
+        // Сортировка вставкой
+        private void InsertionSort()
+        {
+            // Определение делегата сортировки
+            Task insertionSort = new Task(() =>
+            {
+                // Очищение ListBox от предыдущих значений
+                insertionSortListBox.DataSource = null;
+                insertionSortListBox.Invoke(new Action(() => insertionSortListBox.Items.Clear()));
+
+                // Создание нового массива, чтобы в него скопировать сгенерированные числа и в нём совершить сортировку
+                int[] sortedArray = new int[numbers?.Count ?? 0];
+                numbers?.CopyTo(sortedArray);
+
+                // Включение секундомера
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                // Переменные для подсчёта количества сравнений и перестановок
+                long compareCount = 0;
+                long moveCount = 0;
+
+                // Непосредственно сортировка
+                for (int i = 1; i < sortedArray.Length; i++)
+                {
+                    int current = sortedArray[i];
+                    bool flag = false;
+
+                    for (int j = i - 1; j >= 0 && !flag;)
+                    {
+                        compareCount++;
+                        if (current < sortedArray[j])
+                        {
+                            sortedArray[j + 1] = sortedArray[j];
+                            j--;
+                            sortedArray[j + 1] = current;
+                            moveCount++;
+                        }
+                        else flag = true;
+                    }
+                }
+
+                // Остановка секундомера
+                stopwatch.Stop();
+
+                // Вывод сообщения о завершении сортировки и собранных показателях
+                insertionSortLabel.Invoke(new Action(() =>
+                {
+                    insertionSortLabel.Text = "Сортировка вставками завершена!\n" +
+                    "Кол-во сравнений: " + compareCount + ". \n" +
+                    "Кол-во перестановок: " + moveCount + ". \n" +
+                    "Затрачено времени: " + stopwatch.ElapsedMilliseconds + " мс.";
+                    insertionSortLabel.Visible = true;
+                }));
+                
+                // Вывод отсортированного массива
+                insertionSortListBox.Invoke(new Action(() => insertionSortListBox.DataSource = sortedArray));
+            });
+
+            // Запуск сортировки
+            insertionSort.Start();
+        }
+
+        //Сортировка подсчётом
+        private void CountingSort()
+        {
+            // Определение делегата сортировки
+            Task countingSort = new Task(() =>
+            {
+                // Очищение ListBox от предыдущих значений
+                countingSortListBox.DataSource = null;
+                countingSortListBox.Invoke(new Action(() => countingSortListBox.Items.Clear()));
+
+                // Создание нового массива, чтобы в него скопировать сгенерированные числа и в нём совершить сортировку
+                int[] sortedArray = new int[numbers?.Count ?? 0];
+                numbers?.CopyTo(sortedArray);
+
+                // Включение секундомера
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                // Переменные для подсчёта количества сравнений и перестановок
+                long compareCount = 0;
+                long moveCount = 0;
+               
+                // Вспомогательные переменные для сортировки
+                int size = sortedArray.Length;
+                int maxElement = FindMaxVal(sortedArray, size);
+                int[] occurrences = new int[maxElement + 1];
+
+                // Зануление вспомогательного массива
+                for (int i = 0; i < maxElement + 1; i++)
+                {
+                    occurrences[i] = 0;
+                }
+
+                // Подсчёт всех вхождений в массив
+                for (int i = 0; i < size; i++)
+                {
+                    occurrences[sortedArray[i]]++;
+                    moveCount++;
+                }
+
+                // Выстраивание массива в отсортированном виде
+                for (int i = 0, j = 0; i <= maxElement; i++)
+                {
+                    compareCount++;
+                    while (occurrences[i] > 0)
+                    {
+                        sortedArray[j] = i;
+                        j++;
+                        occurrences[i]--;
+                        moveCount++;
+                    }
+                }
+
+                // Остановка секундомера
+                stopwatch.Stop();
+
+                // Вывод сообщения о завершении сортировки и собранных показателях
+                countingSortLabel.Invoke(new Action(() =>
+                {
+                    countingSortLabel.Text = "Сортировка подсчётом завершена!\n" +
+                    "Кол-во сравнений: " + compareCount + ". \n" +
+                    "Кол-во перестановок: " + moveCount + ". \n" +
+                    "Затрачено времени: " + stopwatch.ElapsedMilliseconds + " мс.";
+                    countingSortLabel.Visible = true;
+                }));
+
+                // Вывод отсортированного массива
+                countingSortListBox.Invoke(new Action(() => countingSortListBox.DataSource = sortedArray));
+            });
+
+            // Запуск сортировки
+            countingSort.Start();
+        }
+
+        // Вспомогательная функция поиска максимального значения в массиве
+        private int FindMaxVal(int[] array, int size)
+        {
+            var maxVal = array[0];
+
+            for (int i = 1; i < size; i++)
+            {
+                if (array[i] > maxVal)
+                    maxVal = array[i];
+            }
+
+            return maxVal;
+        }
+
+    }
+}
